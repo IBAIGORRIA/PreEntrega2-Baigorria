45bd623daaf41321530e15cea2dcd9378415520c
Iñaki <ixbaigorriaz@gmail.com>
2023-09-20 14:07:51 -0300

    cambios menores

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-var cantPer = 0;
+let cantPer = 0;
 
-var monTotal = 0;
+let monTotal = 0;
 
 function dejarPropina(){
-    let propina = parseFloat(prompt('Desea Dejar propina?: '));
+    let propina = Number(prompt('Propina: '));
         monTotal = monTotal+propina;
 };
 
 function division(){
         let xPersona = monTotal/cantPer;
-        alert('Deben abonar ' + xPersona.toFixed(2) + ' cada uno.\n Su monto total a abonar es :' + monTotal.toFixed(2) );
+        alert('Deben abonar ' + xPersona.toFixed(2) + ' cada uno.\n El monto total a abonar es :' + monTotal.toFixed(2) );
 };
 
 function ingresarValores(){
-    for (var i=0; i<cantPer; i++){
-        let comida = parseFloat(prompt('Comensal '+ (i+1) +'\n Ingrese valor comida: '));
-        let bebida = parseFloat(prompt('Comensal '+ (i+1) +'\n Ingrese valor bebida: '));
-        let postre = parseFloat(prompt('Comensal '+ (i+1) +'\n Ingrese valor postre: '));
+    for (let i=0; i<cantPer; i++){
+        let comida = Number(prompt('Comensal '+ (i+1) +'\n Ingrese valor comida: '));
+        let bebida = Number(prompt('Comensal '+ (i+1) +'\n Ingrese valor bebida: '));
+        let postre = Number(prompt('Comensal '+ (i+1) +'\n Ingrese valor postre: '));
         monTotal += comida + bebida + postre;
         dejarPropina();
         };
 };
 
-function ActivarMesa(nuMesa) {
-    cantPer= parseInt(prompt('Abriendo mesa' + nuMesa + '\n ¿Cuántas personas son? \n Indique de 1 a 5. Coloque 0 para salir' ));
+function activarMesa(nuMesa) {
+    cantPer= Number(prompt('Abriendo mesa' + nuMesa + '\n ¿Cuántas personas son? \n Indique de 1 a 5. Coloque 0 para salir' ));
     if (cantPer===0) {alert('Gracias por venir!');
     }else if (cantPer >=1 && cantPer<=3){
             ingresarValores();
